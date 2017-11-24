4471b2bfc44004d339943fea88e394cf1605399a
Madhavan, Arulselvan <arulselvan1234@gmail.com>
2017-11-23 18:40:08 -0800

    Rewrote solution for problem#5 with compose

--- exercism_problems/space-age/src/main/scala/SpaceAge.scala
+++ exercism_problems/space-age/src/main/scala/SpaceAge.scala
@@ -3,33 +3,33 @@ import scala.language.postfixOps
 object SpaceAge {
   final val EARTH_DAYS_IN_YEAR = 365.25
 
-  def earthYearInSecs(secs: Double, factor: Double): Double =
+  def earthYearInSecs(secs: Double)(factor: Double): Double =
     (secs / (EARTH_DAYS_IN_YEAR * 24 * 60 * 60)) / factor
 
   def format(d: Double): Double =
     BigDecimal(d) setScale (2, BigDecimal.RoundingMode.HALF_EVEN) toDouble
 
   def onEarth(secs: Double): Double =
-    format(earthYearInSecs(secs, 1.0))
+    (format _ compose (earthYearInSecs _)(secs))(1.0)
 
   def onMercury(secs: Double): Double =
-    format(earthYearInSecs(secs, 0.2408467))
+    (format _ compose (earthYearInSecs _)(secs))(0.2408467)
 
   def onVenus(secs: Double): Double =
-    format(earthYearInSecs(secs, 0.61519726))
+    (format _ compose (earthYearInSecs _)(secs))(0.61519726)
 
   def onMars(secs: Double): Double =
-    format(earthYearInSecs(secs, 1.8808158))
+    (format _ compose (earthYearInSecs _)(secs))(1.8808158)
 
   def onJupiter(secs: Double): Double =
-    format(earthYearInSecs(secs, 11.862615))
+    (format _ compose (earthYearInSecs _)(secs))(11.862615)
 
   def onSaturn(secs: Double): Double =
-    format(earthYearInSecs(secs, 29.447498))
+    (format _ compose (earthYearInSecs _)(secs))(29.447498)
 
   def onUranus(secs: Double): Double =
-    format(earthYearInSecs(secs, 84.016846))
+    (format _ compose (earthYearInSecs _)(secs))(84.016846)
 
   def onNeptune(secs: Double): Double =
-    format(earthYearInSecs(secs, 164.79132))
+    (format _ compose (earthYearInSecs _)(secs))(164.79132)
 }
