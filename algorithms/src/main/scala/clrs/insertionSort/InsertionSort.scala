eda49fa61de1898d097b3bfd06ed005a34d898d8
Madhavan, Arulselvan <arulselvan1234@gmail.com>
2017-11-25 13:02:10 -0800

    Insertion Sort Functional Approach

--- algorithms/src/main/scala/clrs/insertionSort/InsertionSort.scala
+++ algorithms/src/main/scala/clrs/insertionSort/InsertionSort.scala
@@ -1,55 +1,21 @@
 package clrs.insertionSort
-import scala.annotation.tailrec
 
 object InsertionSort {
 
-  // @tailrec
-  // private[this] def findMinIndex[T: Ordering](elem: T,
-  //                                             start: Int,
-  //                                             end: Int,
-  //                                             a: Array[T]): Int = {
-  //   if (start == end) end
-  //   else if (implicitly[Ordering[T]].lt(elem, a(start))) start
-  //   else findMinIndex(elem, start + 1, end, a)
-  // }
-
-  // private[this] def swap[T](src: Int, dest: Int, a: Array[T]): Array[T] = {
-  //   val temp = a(src)
-  //   a(src) = a(dest)
-  //   a(dest) = temp
-  //   a
-  // }
-
-  // private[this] def shiftByOne[T](start:Int, a:Array[T]):Array[T] =
-  //   for (start until a.length) {
-  //     val temp = a(start)
-  //   }
-
-  private[this] def shiftByOne[T](x: T,
-                                  start: Int,
-                                  end: Int,
-                                  a: Array[T]): Array[T] =
-    if (start == end) {
-      a(end) = x
+  def switchElements[T: Ordering](currentIndex: Int, a: Array[T]): Array[T] = {
+    val nextIndex    = currentIndex + 1
+    if (implicitly[Ordering[T]] lt (a(nextIndex), a(currentIndex))) {
+      val currentElem = a(currentIndex)
+      a(currentIndex) = a(nextIndex)
+      a(nextIndex) = currentElem
       a
-    } else {
-      val next = a(start)
-      a(start) = x
-      shiftByOne(next, start + 1, end, a)
-    }
+    } else a
+  }
 
-  def sort[T: Ordering](b: Array[T]): Array[T] = {
-    var a = b
-    for (i <- 1 until a.length) {
-      var j = 0
-      while (j < i) {
-        if (implicitly[Ordering[T]] lt (a(i), a(j))) {
-          a = shiftByOne(a(i), j, i, a)
-          j = i
-        }
-        j += 1
-      }
-    }
-    a
+  def placeElement[T: Ordering](a: Array[T], end: Int): Array[T] =
+    (0 until end).foldRight(a)(switchElements)
+
+  def sort[T: Ordering](a: Array[T]): Array[T] = {
+    (1 until a.length).foldLeft(a)(placeElement)
   }
 }
