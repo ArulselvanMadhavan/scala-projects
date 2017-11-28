4a40c0c7dada703543be831f0923e5f282eff5a6
Madhavan, Arulselvan <arulselvan1234@gmail.com>
2017-11-27 20:19:32 -0800

    Finished the problem on inversions

---
+++ algorithms/src/main/scala/clrs/chapter2/exercises/Inversions.scala
@@ -0,0 +1,42 @@
+package clrs.chapter2.exercises
+
+object Inversions {
+
+  private[this] def combineHelper[T: Ordering](xs: List[T],
+                                               ys: List[T]): (Int, List[T]) =
+    (xs, ys) match {
+      case (Nil, _) => (0, ys)
+      case (_, Nil) => (0, xs)
+      case (x :: l1, y :: l2) =>
+        if (implicitly[Ordering[T]].lteq(x, y)) {
+          val (count, res) = combineHelper(l1, ys)
+          (count, x :: res)
+        } else {
+          val (count, res) = combineHelper(xs, l2)
+          (count + xs.length, y :: res)
+        }
+    }
+
+  private[this] def combine[T: Ordering](
+      left: (Int, List[T]),
+      right: (Int, List[T])): (Int, List[T]) = {
+    val (lcount, xs): (Int, List[T]) = left
+    val (rcount, ys)                 = right
+    val (tcount, zs)                 = combineHelper(xs, ys)
+    (lcount + rcount + tcount, zs)
+  }
+
+  private[this] def sort[T: Ordering](acc: (Int, List[T])): (Int, List[T]) = {
+    val (k, xs) = acc
+    xs match {
+      case Nil      => acc
+      case x :: Nil => acc
+      case _ =>
+        combine(sort((k, xs.slice(0, xs.length / 2))),
+                sort((k, xs.slice(xs.length / 2, xs.length))))
+    }
+  }
+
+  def count[T: Ordering](xs: List[T]): Int =
+    sort((0, xs))._1
+}
