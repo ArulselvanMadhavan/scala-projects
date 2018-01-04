a500c0bdd8f39c605e3d0b2106a64f7fb09ae618
Madhavan, Arulselvan <arulselvan1234@gmail.com>
2018-01-03 17:58:49 -0800

    Long to Int

--- algorithms/src/main/scala/hackerrank/warmup/bigsum.scala
+++ algorithms/src/main/scala/hackerrank/warmup/bigsum.scala
@@ -3,13 +3,13 @@ import java.util.Scanner
 
 object BigSum {
 
-  def computeSum(xs:IndexedSeq[Long]):Long = xs.sum
+  def computeSum(xs:IndexedSeq[Int]):Long = xs.foldLeft(0L)(_ + _)
 
   def main(args:Array[String]):Unit = {
     val sc = new Scanner(System.in)
     val results = for {
-      t <- 0 to sc.nextInt()
-    } yield sc.nextLong()
-    computeSum(results)
+      t <- 0 until sc.nextInt()
+    } yield sc.nextInt()
+    println(computeSum(results))
   }
 }
--- algorithms/src/test/scala/hackerrank/warmup/bigsumSpec.scala
+++ algorithms/src/test/scala/hackerrank/warmup/bigsumSpec.scala
@@ -4,7 +4,7 @@ import org.scalatest.{FunSuite, Matchers}
 
 class BigSumSpec extends FunSuite with Matchers {
   test("should compute the sum") {
-    val data = IndexedSeq(1000000001L,1000000002L,1000000003L,1000000004L,1000000005L);
+    val data = IndexedSeq(1000000001,1000000002,1000000003,1000000004,1000000005);
     val result = BigSum.computeSum(data);
     result should be(5000000015L)
   }
