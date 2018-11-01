0c1ca3225b7ce68a89d0126e71667b3da43ce42b
Arul Madhavan <arul.madhavan@arulmadhavan-MBP-2778.local>
2018-10-31 21:37:39 -0700

    Basic Typeclasses

--- fp-for-mortals/src/main/scala/com/arulselvan/chapter5/Typeclasses.scala
+++ fp-for-mortals/src/main/scala/com/arulselvan/chapter5/Typeclasses.scala
@@ -74,7 +74,60 @@ object Typeclasses {
 
   @typeclass trait Functor[F[_]] {
     def map[A, B](fa: F[A])(f: A => B): F[B]
-    def void[A](fa: F[A]): F[Unit] = map(fa)(_ => ())
+    def void[A](fa: F[A]): F[Unit]                     = map(fa)(_ => ())
+    def fproduct[A, B](fa: F[A])(f: A => B): F[(A, B)] = map(fa)(a => (a, f(a)))
+    def fpair[A](fa: F[A]): F[(A, A)]                  = map(fa)(a => (a, a))
+    def mapply[A, B](a: A)(f: F[A => B]): F[B]         = map(f)((ff: A => B) => ff(a))
   }
 
+  @typeclass trait Foldable[F[_]] {
+    def foldMap[A, B: Monoid](fa: F[A])(f: A => B): B
+    def foldRight[A, B](fa: F[A], z: => B)(f: (A, => B) => B): B
+    def foldLeft[A, B](fa: F[A], z: B)(f: (B, A) => B): B
+  }
+
+  trait Traverse[F[_]] extends Functor[F] with Foldable[F] {
+    def traverse[G[_]: Applicative, A, B](fa: F[A])(f: A => G[B]): G[F[B]]
+    def sequence[G[_]: Applicative, A](fga: F[G[A]]): G[F[A]]
+    def reverse[A](fa: F[A]): F[A]
+    def zipL[A, B](fa: F[A], fb: F[B]): F[(A, Option[B])]
+    def zipR[A, B](fa: F[A], fb: F[B]): F[(Option[A], B)]
+    def indexed[A](fa: F[A]): F[(Int, A)]
+    def zipWithL[A, B, C](fa: F[A], fb: F[B])(f: (A, Option[B]) => C): F[C]
+    def zipWithR[A, B, C](fa: F[A], fb: F[B])(f: (Option[A], B) => C): F[C]
+    def mapAccumL[S, A, B](fa: F[A], z: S)(f: (S, A) => (S, B)): (S, F[B])
+    def mapAccumR[S, A, B](fa: F[A], Z: S)(f: (A, S) => (B, S)): (F[B], S)
+  }
+
+  // Data Encoding of Logical OR
+  sealed abstract class \&/[+A, +B]
+  final case class This[A](aa: A)           extends (A \&/ Nothing)
+  final case class That[B](bb: B)           extends (Nothing \&/ B)
+  final case class Both[A, B](aa: A, bb: B) extends (A \&/ B)
+
+  @typeclass trait Align[F[_]] extends Functor[F] {
+    def alignWith[A, B, C](f: A \&/ B => C): (F[A], F[B]) => F[C]
+    def align[A, B](a: F[A], b: F[B]): F[A \&/ B]
+    def merge[A: Semigroup](a1: F[A], a2: F[A]): F[A]
+    def pad[A, B]: (F[A], F[B]) => F[(Option[A], Option[B])]
+    def padWith[A, B, C](f: (Option[A], Option[B]) => C): (F[A], F[B]) => F[C]
+  }
+
+  object Variance {
+    // Functors
+    // InvariantFunctor
+    @typeclass trait InvariantFunctor[F[_]] {
+      def xmap[A, B](fa: F[A], f: A => B, g: B => A): F[B]
+    }
+    // Covariant Functor
+    @typeclass trait Functor[F[_]] extends InvariantFunctor[F] {
+      def map[A, B](fa: F[A])(f: A => B): F[B]
+      override def xmap[A, B](fa: F[A], f: A => B, g: B => A): F[B] = map(fa)(f)
+    }
+
+    @typeclass trait Contravariant[F[_]] extends InvariantFunctor[F] {
+      def contramap[A, B](fa: F[A])(f: B => A): F[B]
+      override def xmap[A, B](fa: F[A], f: A => B, g: B => A): F[B] = contramap(fa)(g)
+    }
+  }
 }
